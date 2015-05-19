b9d15bdabd7fe9f41991f9f623017947516252ae
Oleksii <lamrak@gmail.com>
2015-05-19 16:40:06 +0300

    init

---
+++ app/src/main/java/com/validcat/myapplication/Comment.java
@@ -0,0 +1,31 @@
+package com.validcat.myapplication;
+
+/**
+ * Created by Dobrunov on 19.05.2015.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
